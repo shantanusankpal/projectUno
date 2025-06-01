9278ece176955e4eb32e73193dc139f0d88aa3a0
Shantanu Sankpal <shantanuasankpal@gmail.com>
2025-06-01 22:25:10 +0530

    Added bean of repository

--- src/main/java/com/example/practice/projectUno/repository/BookRepository.java
+++ src/main/java/com/example/practice/projectUno/repository/BookRepository.java
@@ -3,9 +3,11 @@ package com.example.practice.projectUno.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.practice.projectUno.dto.BookDto;
 
+@Repository
 public interface BookRepository extends JpaRepository<BookDto, Integer> {
   BookDto findByName(String name);
 
