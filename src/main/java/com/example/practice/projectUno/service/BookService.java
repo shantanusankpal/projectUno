fbea2e0063ee41dc8e8764358b5291673f662a28
Shantanu Sankpal <shantanuasankpal@gmail.com>
2025-06-01 22:40:22 +0530

    Changes to service.

--- src/main/java/com/example/practice/projectUno/service/BookService.java
+++ src/main/java/com/example/practice/projectUno/service/BookService.java
@@ -2,7 +2,6 @@ package com.example.practice.projectUno.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.practice.projectUno.dao.BookDao;
@@ -10,13 +9,12 @@ import com.example.practice.projectUno.dto.BookDto;
 import com.example.practice.projectUno.mapper.BookMapper;
 import com.example.practice.projectUno.repository.BookRepository;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class BookService {
 
-  @Autowired
   private final BookRepository bookRepository;
 
   public Boolean addBook(BookDao bookDao) {
